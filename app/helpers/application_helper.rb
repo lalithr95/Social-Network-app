f96de00794359099d9d28b61f2d6921705b12b48
Lalithr95 <lalithr95@gmail.com>
2015-04-02 15:14:13 +0000

    updated routes,layout and users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+        title = "Social networking app"
+        if page_title.empty?
+            title
+        else
+            "#{page_title} | #{title}"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  root 'static_pages#home'
+
+  get 'help' => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
   
-  get 'static_pages/about'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
