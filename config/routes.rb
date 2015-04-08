b8402fc534a964f194a7302bd1a596e1f9e5de30
Lalithr95 <lalithr95@gmail.com>
2015-04-08 13:20:29 +0000

    Password reset updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+
   get 'sessions/new'
 
   get 'users/new'
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :account_activations ,only: [:edit]
-  
+  resources :password_resets ,only: [:new,:create,:edit,:update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
